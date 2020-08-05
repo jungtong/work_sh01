20accfd24fe196ab1b572cae465a0bbdde9bb1c4
jungtong/RND5 <jungtong@idis.co.kr>
2020-08-05 14:21:32 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def hello_world():
 def background_process_test():
     print("background task")
 
+    q = Queue(connection=conn)
     result = q.enqueue(r3unner_main)
     print(result)
     return ("nothing")
